759ec5e9678fe8998330675d66e6e81389172817
Candace Eckels <candace.eckels@gmail.com>
2023-02-16 22:42:48 -0700

    Menu method complete and testing complete

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Potluck
 
   attr_reader :date, :dishes
@@ -15,6 +16,23 @@ class Potluck
     @dishes.map do |dish|
       dish if dish.category == category 
     end.compact
-    
+  end
+
+  def menu
+    menu_hash.each do |category, dishes|
+      dishes.sort!
+    end
+  end
+
+  def menu_hash
+    hash = {}
+    @dishes.each do |dish|
+      if !hash[:"#{dish.category}s"].nil?
+        hash[:"#{dish.category}s"] << dish.name
+      else
+        hash[:"#{dish.category}s"] = [dish.name]
+      end
+    end
+    hash
   end
 end
